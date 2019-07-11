f52d6846c922585a0da017b9322f8cfd28f6755c
SOUHARDYA ADHIKARY <souhardyaadhikary86942@gmail.com>
2019-07-12 00:32:00 +0530

    main activity updated

--- app/src/main/java/com/example/climate/MainActivity.java
+++ app/src/main/java/com/example/climate/MainActivity.java
@@ -1,9 +1,34 @@
 package com.example.climate;
 
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    final String WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather";
+
+    // App ID to use OpenWeather data
+    final String APP_ID = "e72ca729af228beabd5d20e3b7749713";
+
+    // Time between location updates (5000 milliseconds or 5 seconds)
+    final long MIN_TIME = 5000;
+
+    // Distance between location updates (1000m or 1km)
+    final float MIN_DISTANCE = 1000;
+
+    // Set LOCATION_PROVIDER here. Using GPS_Provider for Fine Location (good for emulator):
+    // Recommend using LocationManager.NETWORK_PROVIDER on physical devices (reliable & fast!)
+    final String LOCATION_PROVIDER= LocationManager.GPS_PROVIDER;
+
+
+    // Member Variables:
+
+    TextView mCityLabel;
+    ImageView mWeatherImage;
+    TextView mTemperatureLabel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
