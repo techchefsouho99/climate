a6454a4df09fe5c910aaa82d0e80f463cf1523d8
SOUHARDYA ADHIKARY <souhardyaadhikary86942@gmail.com>
2019-07-13 02:04:26 +0530

    location permission accessed

--- app/src/main/java/com/example/climate/MainActivity.java
+++ app/src/main/java/com/example/climate/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.climate;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -21,8 +29,9 @@ public class MainActivity extends AppCompatActivity {
 
     // Set LOCATION_PROVIDER here. Using GPS_Provider for Fine Location (good for emulator):
     // Recommend using LocationManager.NETWORK_PROVIDER on physical devices (reliable & fast!)
-    final String LOCATION_PROVIDER= LocationManager.GPS_PROVIDER;
+    final String LOCATION_PROVIDER = LocationManager.NETWORK_PROVIDER;
 
+    final int REQUEST_CODE=123;
 
     // Member Variables:
 
@@ -30,12 +39,77 @@ public class MainActivity extends AppCompatActivity {
     ImageView mWeatherImage;
     TextView mTemperatureLabel;
 
+    // Declaring a LocationManager and a LocationListener here:
+    LocationManager mlocationManager;
+    LocationListener mLocationListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
+    }
 
+    // onResume() life cyle callback:
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("clima", "onresume called()");
+        getWeatherForCurrentLocation();
+    }
+
+    private void getWeatherForCurrentLocation() {
+        mlocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        mLocationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                Log.d("clima", "on location cahnged");
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+                Log.d("clima", "on provider disabled");
+            }
+        };
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},REQUEST_CODE);
+            return;
+        }
+        mlocationManager.requestLocationUpdates(LOCATION_PROVIDER, MIN_TIME, MIN_DISTANCE, mLocationListener);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode==REQUEST_CODE){
+            if (grantResults.length >0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                getWeatherForCurrentLocation();
+                Log.d("clima","log granted");
+            }else {
+                Log.d("clima","log denied");
+            }
+        }
     }
 }
