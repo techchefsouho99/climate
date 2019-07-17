382a971e36d3d1e5ba88251d2c25d62f0a07a2e4
SOUHARDYA ADHIKARY <souhardyaadhikary86942@gmail.com>
2019-07-17 21:27:17 +0530

    Splash activity added

--- app/src/main/java/com/example/climate/SplashActivity.java
+++ app/src/main/java/com/example/climate/SplashActivity.java
@@ -1,4 +1,24 @@
 package com.example.climate;
 
-public class SplashActivity {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+
+public class SplashActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent=new Intent(SplashActivity.this,MainActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+            }
+        },3000);
+    }
 }
