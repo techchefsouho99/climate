b7128b0c73fddc77b568cc09cc3377acaa6587cd
SOUHARDYA ADHIKARY <souhardyaadhikary86942@gmail.com>
2019-07-18 00:48:26 +0530

    web page integrated

--- app/src/main/java/com/example/climate/MainActivity.java
+++ app/src/main/java/com/example/climate/MainActivity.java
@@ -2,12 +2,14 @@ package com.example.climate;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -15,6 +17,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -25,9 +29,9 @@ public class MainActivity extends AppCompatActivity implements LocationListener{
 
 
     TextView mCityLabel;
+    ImageButton web;
 
     LocationManager locationManager;
-
     int c=1;
 
     @Override
@@ -36,11 +40,12 @@ public class MainActivity extends AppCompatActivity implements LocationListener{
         setContentView(R.layout.activity_main);
         mCityLabel=(TextView)this.findViewById(R.id.locationTV);
         mCityLabel.setSelected(true);
-
+        web=(ImageButton)findViewById(R.id.changeCityButton);
         if (ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 
             ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION}, 101);
         }
+
 
     }
     @Override
@@ -61,12 +66,24 @@ public class MainActivity extends AppCompatActivity implements LocationListener{
         }
     }
 
+
     @Override
-    public void onLocationChanged(Location location) {
+    public void onLocationChanged(final Location location) {
         try{
         Geocoder geocoder = new Geocoder(this, Locale.getDefault());
         List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+        Log.d("climate",""+location.getLatitude());
+        Log.d("climate",""+location.getLongitude());
         mCityLabel.setText(addresses.get(0).getAddressLine(0));
+
+        web.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri= Uri.parse("https://www.windy.com/-Temperature-temp?temp,"+location.getLatitude()+","+location.getLongitude());
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
 
         }
         catch(Exception e) {
