8a8f074a174ab831ee2c0efd486ce53fac2c3335
SOUHARDYA ADHIKARY <souhardyaadhikary86942@gmail.com>
2019-07-21 00:10:01 +0530

    tabbed activity updated

--- app/src/main/java/com/example/climate/SectionsPagerAdapter.java
+++ app/src/main/java/com/example/climate/SectionsPagerAdapter.java
@@ -14,7 +14,7 @@ import android.support.v4.app.FragmentPagerAdapter;
 public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
     @StringRes
-    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2};
+    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2,R.string.tab_string_3};
     private final Context mContext;
 
     public SectionsPagerAdapter(Context context, FragmentManager fm) {
@@ -38,6 +38,6 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
     @Override
     public int getCount() {
         // Show 2 total pages.
-        return 2;
+        return 3;
     }
 }
--- app/src/main/java/com/example/climate/TabbedActivity.java
+++ app/src/main/java/com/example/climate/TabbedActivity.java
@@ -29,4 +29,5 @@ public class TabbedActivity extends AppCompatActivity {
             }
         });
     }
+
 }
