8da253ebb29671277256b4ac9b1cd6f6f0b7f50f
SOUHARDYA ADHIKARY <souhardyaadhikary86942@gmail.com>
2019-07-21 00:20:50 +0530

    menu is integrated

--- app/src/main/java/com/example/climate/TabbedActivity.java
+++ app/src/main/java/com/example/climate/TabbedActivity.java
@@ -1,11 +1,16 @@
 package com.example.climate;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.AttributeSet;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 public class TabbedActivity extends AppCompatActivity {
@@ -29,5 +34,26 @@ public class TabbedActivity extends AppCompatActivity {
             }
         });
     }
+    @Override
+    public View onCreateView(String name, Context context, AttributeSet attrs) {
+        return super.onCreateView(name, context, attrs);
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.tab,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return true;
+    }
 }
