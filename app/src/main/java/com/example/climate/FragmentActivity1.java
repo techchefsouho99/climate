7cbe91eda6bc9b9776a88977a204390148b1c798
SOUHARDYA ADHIKARY <souhardyaadhikary86942@gmail.com>
2019-07-22 18:42:33 +0530

    first fragment is updated

--- app/src/main/java/com/example/climate/FragmentActivity1.java
+++ app/src/main/java/com/example/climate/FragmentActivity1.java
@@ -1,19 +1,43 @@
 package com.example.climate;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class FragmentActivity1 extends Fragment {
+import java.util.List;
+import java.util.Locale;
+
+public class FragmentActivity1 extends Fragment  implements LocationListener{
 
     private ImageButton button1;
+
+    TextView mCityLabel;
+    TextView web;
+
+    LocationManager locationManager;
+    int c=1;
 
     @Nullable
     @Override
@@ -28,7 +52,74 @@ public class FragmentActivity1 extends Fragment {
                 Toast.makeText(getActivity(),"1",Toast.LENGTH_SHORT).show();
             }
         });
+        mCityLabel=(TextView)view.findViewById(R.id.locationTV);
+        mCityLabel.setSelected(true);
+        web=(TextView)view.findViewById(R.id.tempTV);
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION}, 101);
+        }
         //return super.onCreateView(inflater, container, savedInstanceState);
         return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if(c==1){
+            getLocation();
+            c++;
+        }
+    }
+    void getLocation() {
+
+        try {
+            locationManager = (LocationManager)getActivity().getSystemService(Context.LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,1000,5, (LocationListener) this);
+        }
+
+        catch(SecurityException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void onLocationChanged(final Location location) {
+        try{
+            Geocoder geocoder = new Geocoder(getActivity(), Locale.getDefault());
+            List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+            Log.d("climate",""+location.getLatitude());
+            Log.d("climate",""+location.getLongitude());
+            mCityLabel.setText(addresses.get(0).getAddressLine(0));
+
+            web.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Uri uri= Uri.parse("https://www.windy.com/-Temperature-temp?temp,"+location.getLatitude()+","+location.getLongitude());
+                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                    startActivity(intent);
+                }
+            });
+
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+        Toast.makeText(getActivity(),"Please Enable GPS and Internet", Toast.LENGTH_SHORT);
     }
 }
--- app/src/main/java/com/example/climate/TabbedActivity.java
+++ app/src/main/java/com/example/climate/TabbedActivity.java
@@ -24,15 +24,15 @@ public class TabbedActivity extends AppCompatActivity {
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
-        FloatingActionButton fab = findViewById(R.id.fab);
+        /*FloatingActionButton fab = findViewById(R.id.fab);*/
 
-        fab.setOnClickListener(new View.OnClickListener() {
+        /*fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
     }
     @Override
     public View onCreateView(String name, Context context, AttributeSet attrs) {
