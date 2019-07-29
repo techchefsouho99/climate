e9fa4753cfd0e4080cc0ac3263558b4989f12c19
SOUHARDYA ADHIKARY <souhardyaadhikary86942@gmail.com>
2019-07-29 20:04:41 +0530

    frag3.java updated

--- app/src/main/java/com/example/climate/FragmentActivity3.java
+++ app/src/main/java/com/example/climate/FragmentActivity3.java
@@ -1,12 +1,16 @@
 package com.example.climate;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -19,7 +23,7 @@ public class FragmentActivity3 extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_fragment3, container, false);
-
+        Log.d("Clima","hello");
         /*button3 = (Button) view.findViewById(R.id.frag_button3);
         button3.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,6 +32,16 @@ public class FragmentActivity3 extends Fragment {
             }
         });*/
         //return super.onCreateView(inflater, container, savedInstanceState);
+        SharedPreferences sharedPreferences=getActivity().getPreferences(Context.MODE_PRIVATE);
+        float lat= (float) sharedPreferences.getFloat("lat", 2);
+        String lati=String.valueOf(lat);
+        float lon= (float) sharedPreferences.getFloat("long",2);
+        String longi=String.valueOf(lon);
+        Log.d("Clima",""+lati);
+        Log.d("Clima",""+longi);
+
+        WebView webView=(WebView)view.findViewById(R.id.webview);
+        webView.loadUrl("https://www.windy.com/-Temperature-temp?temp,18.857,86.561,7");
         return view;
     }
 
