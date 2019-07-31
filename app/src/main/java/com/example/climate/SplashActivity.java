b487c29db45276ee28879f4ecbd672ae717be825
SOUHARDYA ADHIKARY <souhardyaadhikary86942@gmail.com>
2019-07-31 19:14:28 +0530

    animated splash screen created

--- app/src/main/java/com/example/climate/SplashActivity.java
+++ app/src/main/java/com/example/climate/SplashActivity.java
@@ -4,14 +4,23 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.PersistableBundle;
-import android.support.annotation.Nullable;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SplashActivity extends Activity {
+    ImageView imageView;
+    TextView textView;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        textView=(TextView)findViewById(R.id.creator);
+        imageView=(ImageView)findViewById(R.id.splash_image);
+        Animation animation= AnimationUtils.loadAnimation(this,R.anim.image_animation);
+        imageView.startAnimation(animation);
+        textView.startAnimation(animation);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -19,6 +28,6 @@ public class SplashActivity extends Activity {
                 SplashActivity.this.startActivity(mainIntent);
                 SplashActivity.this.finish();
             }
-        },1500);
+        },2500);
     }
 }
